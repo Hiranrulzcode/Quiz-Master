d3011797512f21522bd7608f1d1a5db9578b3f54
Hiranrulzcode <hiranmayeesaravanan15@gmail.com>
2024-06-30 09:29:53 +0200

    quiz.py

--- quiz.py
+++ quiz.py
@@ -5,24 +5,76 @@ HEIGHT=650
 TITLE="Quiz Master"
 
 question_box=Rect(10,20,650,150)
-answer_box1=Rect(20,270,300,150)
-answer_box2=Rect(10,330,150,150)
-answer_box3=Rect(40,30,150,150)
-answer_box4=Rect(40,50,150,150)
+answer_box1=Rect(20,200,300,150)
+answer_box2=Rect(20,420,300,150)
+answer_box3=Rect(350,200,300,150)
+answer_box4=Rect(350,420,300,150)
+timer_box=Rect(680,20,150,150)
+skip_box=Rect(680,200,150,420)
+
+answer_boxes=[answer_box1, answer_box2, answer_box3, answer_box4]
+
+time_left=10
+score=0
+question_file_name="questions.txt"
+is_game_over=False
+questions=[]
+question_count=0
+question_index=0
+
 
 
 def draw():
     screen.fill("black")
     screen.draw.filled_rect(question_box,(250, 147, 95))
-    screen.draw.filled_rect(answer_box1,(3, 177, 252))
-    screen.draw.filled_rect(answer_box2,(3, 177, 252))
-    screen.draw.filled_rect(answer_box3,(3, 177, 252))
-    screen.draw.filled_rect(answer_box4,(3, 177, 252))
+    screen.draw.filled_rect(timer_box,(7, 245, 138))
+    screen.draw.filled_rect(skip_box,(48, 112, 2))
+
+    for answer_box in answer_boxes:
+        screen.draw.filled_rect(answer_box,(3, 177, 252))
+
+    screen.draw.textbox(str(time_left),timer_box, color="black")
+    screen.draw.textbox("skip",skip_box,color="black",angle=-90)
+    screen.draw.textbox(question[0].strip(),question_box,color="black")
+    index=1
+    for answer_box in answer_boxes:
+        screen.draw.textbox(question[index].strip(),answer_box,color="black")
+        index=index+1
 
 
+def update():
+    pass
 
 
+def read_question_file():
+    global question_count, questions
+    q_file=open(question_file_name, "r")
 
+    for question in q_file:
+        questions.append(question)
+        question_count=question_count+1
+
+    q_file.close()
+
+
+    
+
+read_question_file()
+print(questions)
+
+def read_next_question():
+    global question_index
+    question_index=question_index+1
+    return questions.pop(0).split(",")
+
+question=read_next_question()
+
+def update_time_left():
+    global time_left
+    if time_left:
+        time_left=time_left-1
+
+clock.schedule_interval(update_time_left, 1)
 
 
 
